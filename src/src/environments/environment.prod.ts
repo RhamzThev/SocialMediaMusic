5a1c5ffb6fa67f718770354b30c439b506b0a1c2
RhamzThev <rhamzthev@gmail.com>
2022-12-17 11:42:47 -0500

    downgraded angular to 14. finally added firebase to project

---
+++ src/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'social-media-music',
+    appId: '1:389899955302:web:560eed9dd11235c9a991a7',
+    storageBucket: 'social-media-music.appspot.com',
+    apiKey: 'AIzaSyAk2GqPOWce9o1y7mEs37e7Cz38zico2QE',
+    authDomain: 'social-media-music.firebaseapp.com',
+    messagingSenderId: '389899955302',
+  },
+  production: true
+};
---
+++ src/src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    projectId: 'social-media-music',
+    appId: '1:389899955302:web:560eed9dd11235c9a991a7',
+    storageBucket: 'social-media-music.appspot.com',
+    apiKey: 'AIzaSyAk2GqPOWce9o1y7mEs37e7Cz38zico2QE',
+    authDomain: 'social-media-music.firebaseapp.com',
+    messagingSenderId: '389899955302',
+  },
+  production: false
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
